3677f3ac9dfae843e7a069044f016c77f165d027
tvconss <tranvanconkg@gmail.com>
2025-06-19 21:52:24 +0700

    first commit

---
+++ src/main/java/com/tvconss/vlutedocumentmanagment/VluteDocumentManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.tvconss.vlutedocumentmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VluteDocumentManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VluteDocumentManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tvconss/vlutedocumentmanagment/VluteDocumentManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvconss.vlutedocumentmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VluteDocumentManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
